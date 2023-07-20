8f0b16d4683f7799a635ed3254e5d7c5ddaaed56
sonnh7289 <124328469+sonnh7289@users.noreply.github.com>
2023-07-20 14:30:48 +0700

    Add files via upload

---
+++ static/model.py
@@ -0,0 +1,23 @@
+import json
+import mysql.connector
+
+HOST = "localhost"
+USER = "root"
+PASSWD = "mcso@123#@!"
+DB = "MYANIMELIST"
+
+Anime_Manga_News = r"/root/son/mangareader/Anime_Manga_News.json"
+Comment_News = r"/root/son/mangareader/Comment_News.json"
+Reviews_Anime = r"/root/son/mangareader/Reviews_Anime.json"
+Reviews_Manga = r"/root/son/mangareader/Reviews_Manga.json"
+Reviews_Top_Anime = r"/root/son/mangareader/Reviews_Top_Anime.json"
+Reviews_Top_Manga = r"/root/son/mangareader/Reviews_Top_Manga.json"
+Top_Anime = "/root/son/mangareader/Top_Anime.json"
+Top_Manga = r"/root/son/mangareader/Top_Manga.json"
+
+
+
+def get_data_json(_LINK_JSON_FILE):
+    with open(_LINK_JSON_FILE, "r", encoding="utf-8") as f:
+        data = json.load(f)
+    return data
